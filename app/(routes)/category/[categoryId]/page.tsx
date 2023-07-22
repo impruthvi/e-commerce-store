ef129ee03ba841b9f5b9ea148d2a6174853e83f2
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-07-22 13:11:33 +0530

    implemented a filters for mobile screen

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -7,6 +7,7 @@ import Container from "@/components/ui/container";
 import Filter from "./components/filter";
 import NoResults from "@/components/ui/no-results";
 import ProductCard from "@/components/ui/product-card";
+import MobileFilters from "./components/mobile-filters";
 
 interface CategoryProps {
   params: {
@@ -35,7 +36,7 @@ const Category: React.FC<CategoryProps> = async ({ params, searchParams }) => {
         <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-            {/* Add Mobile Filters */}
+            <MobileFilters sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
